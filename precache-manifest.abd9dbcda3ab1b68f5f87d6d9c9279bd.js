fcb39b849ee72989616f52acdcb0d93032702edd
Karol Dzialowski <karlososhd@gmail.com>
2020-04-27 13:08:17 +0200

    Updates

---
+++ precache-manifest.abd9dbcda3ab1b68f5f87d6d9c9279bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac45584790bebc5a98d8d0ae070b2b2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a788b1f665275a72df95",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cc72920ffeef21f4ee78",
+    "url": "/my-app/static/js/2.07af7b9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.07af7b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a788b1f665275a72df95",
+    "url": "/my-app/static/js/main.4b5a6f4c.chunk.js"
+  },
+  {
+    "revision": "b8bb6c1c0b90715ad49a",
+    "url": "/my-app/static/js/runtime-main.657902a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);

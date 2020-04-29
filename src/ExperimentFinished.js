9ccb7111ace21c5bfc9e7477148ae1185c713142
Karol Dzialowski <karlososhd@gmail.com>
2020-04-29 19:34:52 +0200

    Save experiment data to csv

--- src/ExperimentFinished.js
+++ src/ExperimentFinished.js
@@ -1,13 +1,21 @@
-
 import React from 'react'
+import { CSVLink, CSVDownload } from 'react-csv'
 
-const ExperimentFinished = () => {
+const ExperimentFinished = ({ experimentData }) => {
   return (
     <section class='hero is-fullheight is-default is-bold experiment-step'>
       <div class='container has-text-centered'>
         <div class='columns is-vcentered is-centered'>
           <div class='column is-half'>
               Eksperyment się zakończył
+            <CSVLink
+              filename='technika_pojedynczego_bodzca.csv'
+              className='button'
+              data={experimentData.slice(4, experimentData.length)}
+              // enclosingCharacter=''
+            >
+               Pobierz CSV
+            </CSVLink>
           </div>
         </div>
       </div>
